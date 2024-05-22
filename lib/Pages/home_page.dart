c01a43549bad5e924a7f177dd875f8335c79b272
Swastik Patel <dawnsp0456@gmail.com>
2024-05-22 16:34:51 +0530

    added the done and undone feature

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -9,26 +9,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["Complete the first project!", false],
+    ["Complete the first project!", false],
+    ["Complete the first project!", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 168, 139, 247),
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          centerTitle: true,
-          title: const Text(
-            'To Do',
-            style: TextStyle(color: Colors.white),
-          ),
+      backgroundColor: const Color.fromARGB(255, 168, 139, 247),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text(
+          'To Do',
+          style: TextStyle(color: Colors.white),
         ),
-        body: ListView(
-          children: [
-            ToDoTile(
-              taskName: "Complete the first project!",
-              taskCompleted: true,
-              onChanged: (p0) {},
-            ),
-          ],
-        ));
+      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context, index) {
+            return ToDoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+            );
+          }),
+    );
   }
 }
